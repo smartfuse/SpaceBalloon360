513f4e9a507780365af385f3ca1c8933c97ab208
Lucas Yan <lucasyan@berkeley.edu>
2018-06-12 16:28:14 -0700

    Test

--- ez-wifibroadcast/deploy.sh
+++ ez-wifibroadcast/deploy.sh
@@ -2,9 +2,9 @@
 
 echo "Fetching changes from repo"
 
-make clean
 git pull
 cd wifibroadcast
+make clean
 make
 cd ..
 rm -rf ~/wifibroadcast
@@ -12,13 +12,13 @@ ln -s `pwd`/wifiboadcast ~/wifibroadcast
 rm -rf ~/.dotprofile
 cp -R dotprofile ~/.profile
 
-if [ $1 == 'transmit' ] || [ $1 == 'receive' ]; then
+if [ "$1" == "transmit" ] || [ "$1" == "receive" ]; then
 	echo $1
 	rm -rf /boot/wifibroadcast-1.txt
 	cp /boot/wifibroadcast-1.txt
 fi
 
-if [ $1 == "receive_udp" ]; then
+if [ "$1" == "receive_udp" ]; then
 	echo $1
 	rm -rf /boot/wifibroadcast-udp.txt
 	cp /boot/wifibroadcast-1.txt
