26de3c76efdfc0870d0f935035f3a37ec4b5b72f
John Boiles <johnaboiles@gmail.com>
2018-12-04 12:57:42 -0800

    Add input-ipc-server to mpv run.sh in groundstation

--- ground-station/scripts/run.sh
+++ ground-station/scripts/run.sh
@@ -11,4 +11,4 @@ cd /wifibroadcast
 # TODO save these binaries
 /wifibroadcast/sharedmem_init_rx 
 export DISPLAY=:0
-/wifibroadcast/rx -d 100 -p 0 -d 1 -b 24 -r 12 -f 768 $WLAN | mpv --fs -fps 200 -
+/wifibroadcast/rx -d 100 -p 0 -d 1 -b 24 -r 12 -f 768 $WLAN | mpv --fs -fps 200 --input-ipc-server=/tmp/mpvsocket -
