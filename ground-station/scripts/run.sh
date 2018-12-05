b0e5fe1bcdaca830395b89db5bcd63ac97d6d966
John Boiles <johnaboiles@gmail.com>
2018-12-04 16:52:13 -0800

    Desktop script for starting the groundstation

--- ground-station/scripts/run.sh
+++ ground-station/scripts/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+#
+# Start the groundstation. Must be run as root/sudo
+#
 
+# Multi-rx-server info
 SERVER_HOST=35.161.202.130
 SERVER_PORT=6000
 
@@ -13,7 +17,8 @@ ifconfig $WLAN up
 iwconfig $WLAN channel 13 
 
 # Fire up wifibroadcast!
-/SpaceBalloon360/ez-wifibroadcast/wifibroadcast/sharedmem_init_rx
+WIFIBROADCAST_PATH=/SpaceBalloon360/ez-wifibroadcast/wifibroadcast
+$WIFIBROADCAST_PATH/sharedmem_init_rx
 export DISPLAY=:0
-/SpaceBalloon360/ez-wifibroadcast/wifibroadcast/rx -p 0 -d 1 -b 24 -r 12 -f 768 -s $SERVER_HOST -n $SERVER_HOST $WLAN | mpv --fs -fps 200 --input-ipc-server=/tmp/mpvsocket -
-/SpaceBalloon360/ez-wifibroadcast/wifibroadcast/onscreen_display_server | socat - /tmp/mpvsocket
+$WIFIBROADCAST_PATH/SpaceBalloon360/ez-wifibroadcast/wifibroadcast/rx -p 0 -d 1 -b 24 -r 12 -f 768 -s $SERVER_HOST -n $SERVER_HOST $WLAN | mpv --fs -fps 200 --input-ipc-server=/tmp/mpvsocket -
+$WIFIBROADCAST_PATH/SpaceBalloon360/ez-wifibroadcast/wifibroadcast/onscreen_display_server | socat - /tmp/mpvsocket
