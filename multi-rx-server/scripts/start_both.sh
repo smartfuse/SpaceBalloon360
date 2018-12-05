2ff06cb51d153f6ff51c05aaa32c43deae6b10a5
John Boiles <johnaboiles@gmail.com>
2018-12-04 16:21:40 -0800

    Clean up server run scripts

---
+++ multi-rx-server/scripts/start_both.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rx -p 0 -d 100 -b 24 -r 12 -f 768 -s 0.0.0.0 -u 6000 | tee >(ffmpeg -f h264 -i - -i /home/xmap_insta360.pgm -i /home/ymap_insta360.pgm -filter_complex remap -vcodec h264 -b:v 3000k -strict experimental -f flv rtmp://space.prscp.net/live/payload360) | ffmpeg -f h264 -i - -vcodec copy -strict experimental -f flv rtmp://space.prscp.net/live/payload360unstitched
