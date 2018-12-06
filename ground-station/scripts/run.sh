ab38bac2b747c0bac98b553aa36b1c8ebf6ed52f
John Boiles <johnaboiles@gmail.com>
2018-12-05 22:31:25 -0800

    Resolve SERVER_HOST to IP address for use with rx

--- ground-station/scripts/run.sh
+++ ground-station/scripts/run.sh
@@ -5,7 +5,9 @@
 
 # Multi-rx-server info
 SERVER_HOST=space.johnboiles.com
+SERVER_IP=$(dig +short $SERVER_HOST)
 SERVER_PORT=6000
+echo "Connecting to $SERVER_IP on port $SERVER_PORT"
 
 WLAN=$(iw dev | awk '$1=="Interface"{print $2}')
 echo "Using interface $WLAN"
@@ -20,6 +22,6 @@ iwconfig $WLAN channel 13
 WIFIBROADCAST_PATH=/SpaceBalloon360/ez-wifibroadcast/wifibroadcast
 $WIFIBROADCAST_PATH/sharedmem_init_rx
 export DISPLAY=:0
-$WIFIBROADCAST_PATH/rx -p 0 -d 1 -b 24 -r 12 -f 768 -s $SERVER_HOST -n $SERVER_PORT $WLAN | mpv --fs -fps 200 --input-ipc-server=/tmp/mpvsocket - &
+$WIFIBROADCAST_PATH/rx -p 0 -d 1 -b 24 -r 12 -f 768 -s $SERVER_IP -n $SERVER_PORT $WLAN | mpv --fs -fps 200 --input-ipc-server=/tmp/mpvsocket - &
 sleep 10
 $WIFIBROADCAST_PATH/onscreen_display_server | socat - /tmp/mpvsocket
